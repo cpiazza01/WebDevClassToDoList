bab302a83070d76ac43d9b12f4dc4fa1d6d89bf7
Cody Piazza <codypiazza@gmail.com>
2020-07-30 19:13:59 -0400

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -123,6 +123,6 @@ app.get("/:customListName", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started successfully!");
 });
